06304fc4746690ff58f01aef443ed225c89fc105
Caleb Martin <cam228@outlook.com>
2023-10-11 21:28:24 -0400

    Refining, vacation activity

--- Assets/Scripts/Activities/Farm.cs
+++ Assets/Scripts/Activities/Farm.cs
@@ -31,26 +31,36 @@ public class Farm : Activity
         switch(checkSuccess())
         {
             case 0:
+                //Result Text
                 rText.text = "Did a poor job at the farm...\n\n"+
-                    "Constitution: " + Manager.constitution + " + " + (Constants.statSmall*multiplier*0.5f) + "\n" +
+                    "Constitution: " + Manager.constitution + " + " + (Constants.statTiny*multiplier*0.5f) + "\n" +
                     "Physical Talent: " + Manager.physical + " + " + (Constants.statTiny*multiplier*0.5f) + "\n" +
+                    "Grace: " + Manager.grace + " - " + (Constants.statTiny*multiplier) + "\n" +
                     "Gold: " + Manager.gold + " + 5\n" +
-                    "Stress: " + Manager.stress + " + " + (Constants.statMedium*multiplier*1.5f);
-                Manager.gold = Manager.gold + 5;
-                Manager.constitution += Constants.statSmall*multiplier*0.5f;
+                    "Stress: " + Manager.stress + " + " + (Constants.statMedium*multiplier);
+                
+                //Stat Change
+                Manager.constitution += Constants.statTiny*multiplier*0.5f;
                 Manager.physical += Constants.statTiny*multiplier*0.5f;
-                Manager.stress = Manager.stress + Constants.statMedium*multiplier*1.5f;
+                Manager.grace -= Constants.statTiny*multiplier;
+                Manager.gold += 5;
+                Manager.stress += Constants.statMedium*multiplier;
                 break;
             case 1:
+                //Result Text
                 rText.text = "Did a good job at the farm.\n\n"+
-                    "Constitution: " + Manager.constitution + " + " + (Constants.statSmall*multiplier) + "\n" +
+                    "Constitution: " + Manager.constitution + " + " + (Constants.statTiny*multiplier) + "\n" +
                     "Physical Talent: " + Manager.physical + " + " + (Constants.statTiny*multiplier) + "\n" +
+                    "Grace: " + Manager.grace + " - " + (Constants.statTiny*multiplier) + "\n" +
                     "Gold: " + Manager.gold + " + 10\n" +
                     "Stress: " + Manager.stress + " + " + (Constants.statMedium*multiplier);
-                Manager.gold = Manager.gold + 10;
-                Manager.constitution += Constants.statSmall*multiplier;
+                
+                //Stat Change
+                Manager.constitution += Constants.statTiny*multiplier;
                 Manager.physical += Constants.statTiny*multiplier;
-                Manager.stress = Manager.stress + Constants.statMedium*multiplier;
+                Manager.grace -= Constants.statTiny*multiplier;
+                Manager.gold += 10;
+                Manager.stress += Constants.statMedium*multiplier;
                 break;
             default:
                 Debug.Log("Activity Success Logic Error");
--- Assets/Scripts/ScheduleManager.cs
+++ Assets/Scripts/ScheduleManager.cs
@@ -66,11 +66,18 @@ public class ScheduleManager : MonoBehaviour
         //The queuedActivity should be set by the buttons under the _Opt panels in the scene
         switch(queuedType)
         {
+            /*case "Church":
+                break;
+            case "Babysitting":
+                break;*/
             case "Farm":
                 newActivity = new Farm();
                 break;
             case "Relax":
                 newActivity = new Relax();
+                break;
+            case "Vacation":
+                newActivity = new Vacation();
                 break;
             default:
                 return;
@@ -155,6 +162,12 @@ public class ScheduleManager : MonoBehaviour
     {
         //Show the results
         resultPanel.SetActive(true);
+
+        //Monthly expenses
+        rText.text = "Paid 30G for this month's expenses";
+        Manager.gold -= 30;
+        yield return new WaitForSeconds(1);
+
         //For each activity in the schedule
         for(int i=0; i<4; i++)
         {
@@ -166,6 +179,8 @@ public class ScheduleManager : MonoBehaviour
                 //Sickness penalty
                 if(Manager.sick)
                 {
+                    Manager.trust -= Constants.statTiny;
+
                     rText.text += "\n\n<color=red>Sick\n";
                     switch(Random.Range(1,11))
                     {
@@ -193,6 +208,8 @@ public class ScheduleManager : MonoBehaviour
                             break;
                     }
                 }
+
+                genManager.VerifyStats();
 
                 //Pause to show the player the results
                 yield return new WaitForSeconds(1);
